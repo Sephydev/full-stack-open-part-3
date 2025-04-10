36ced69269f4a385b65c32147a0cb6715804f3ac
Sephydev <nicolas.bussiere@outlook.fr>
2025-04-10 18:17:53 +0200

    Display res.body in log of morgan

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('tiny'))
 app.use(express.json())
 
-
+morgan.token('data', function (request, response) { return JSON.stringify(request.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 
 let persons = [
